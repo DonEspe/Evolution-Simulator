af222e4d2ad4dc4160d7cea130c884e9eff91047
Don Espe <Don.Espe@gmail.com>
2023-11-27 18:06:24 -0600

    Initial Commit

---
+++ Evolution Simulator/Evolution_SimulatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Evolution_SimulatorApp.swift
+//  Evolution Simulator
+//
+//  Created by Don Espe on 11/27/23.
+//
+
+import SwiftUI
+
+@main
+struct Evolution_SimulatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
